1132cfb5a4665b4cccc1820b44aca2fe7d5b2d03
mclk623 <66455829+mclk623@users.noreply.github.com>
2022-03-25 09:37:47 +0800

    Delete 葡萄酒 directory

--- 葡萄酒/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/container
-# Replace Startup Variables
-MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
-echo -e ":/home/container$ ${START_COMMAND}"
-
-# Run the Server
-${MODIFIED_STARTUP}
