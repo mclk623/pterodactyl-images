1a20695877c766d8869a1864753dd143f671bba7
mclk623 <66455829+mclk623@users.noreply.github.com>
2022-10-05 10:51:27 +0800

    Create entrypoint.sh

---
+++ zulu18/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/container
+# 修改变量
+MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
+echo -e ":/home/container$ ${START_COMMAND}"
+# 启动服务器
+${MODIFIED_STARTUP}
