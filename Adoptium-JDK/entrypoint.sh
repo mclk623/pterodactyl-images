b295e4d26ec14e6bb8266d2755d6a38cbbb1fa9b
mclk623 <66455829+mclk623@users.noreply.github.com>
2023-02-04 10:55:26 +0800

    Update entrypoint.sh

--- Adoptium-JDK/entrypoint.sh
+++ Adoptium-JDK/entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
 echo -e ":/home/container$ ${START_COMMAND}"
-export JAVA_HOME=/usr/local/Adoptium-JDK/jdk-19.0.1+10
+export JAVA_HOME=/usr/local/Adoptium-JDK/jdk-19.0.2+7
 export CLASSPATH=.:$JAVA_HOME/jre/lib/rt.jar:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
 export  PATH=$PATH:$JAVA_HOME/bin
 #显示JAVA版本
