6fc597a4b5438cb7404770e3677ef8c180a84af1
mclk623 <66455829+mclk623@users.noreply.github.com>
2023-04-30 15:24:01 +0800

    Update entrypoint.sh

--- azul19/entrypoint.sh
+++ azul19/entrypoint.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
-#JAVA信息
-java -version
+#!/bin/sh
 cd /home/container
-# 修改变量
-MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
-echo -e ":/home/container$ ${START_COMMAND}"
-# 启动服务器
+
+# Output Current Java Version
+java -version ## only really needed to show what version is being used. Should be changed for different applications
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
 ${MODIFIED_STARTUP}
