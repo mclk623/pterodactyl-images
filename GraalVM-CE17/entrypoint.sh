960e7a6132ff1c6631b26430313f475460ede34f
mclk623 <66455829+mclk623@users.noreply.github.com>
2023-05-14 09:56:29 +0800

    Create entrypoint.sh

---
+++ GraalVM-CE17/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/container
+# 修改变量
+MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
+echo -e ":/home/container$ ${START_COMMAND}"
+#显示JAVA版本
+java -version
+# 启动服务器
+${MODIFIED_STARTUP}
